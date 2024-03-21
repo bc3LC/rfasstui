48f77b50003393eb73da7cf0391a5d95948d3c86
klau506 <claudia.rodes@bc3research.org>
2024-03-21 08:26:01 +0100

    Add google site

---
+++ inst/rfasstui/www/google-analytics.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-HTQ5KFV0W3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-HTQ5KFV0W3');
+</script>
